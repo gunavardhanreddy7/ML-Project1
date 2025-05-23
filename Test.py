fa19f214fa14037432630861a3e9920c2c4dd459
GUNAVARDHAN JAKKIDI <vardhanreddy226@gmail.com>
2025-05-22 22:26:50 -0400

    Configuration_Commit

--- Test.py
+++ Test.py
@@ -1,1 +1,40 @@
-import 
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "ML-Project1"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "template/index.html"
+]
+
+for files in list_of_files:
+    file_path = Path(files)
+    file_dir, file_name = os.path.split(file_path)
+    if file_dir != "":  
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating directory; {file_dir} for the file: {file_name}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {file_path}")
